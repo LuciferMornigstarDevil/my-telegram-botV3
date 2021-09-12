e37a30fc8d8b53acbe4d87d4128e3256954fa7a0
Jinn <85754819+Jinn-of-telegram@users.noreply.github.com>
2021-09-12 15:28:15 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -136,7 +136,7 @@ async def start(bot, update):
                 update.from_user.first_name),
         reply_markup=reply_markup,
         parse_mode="html",
-       reply_to_message_id=update.message_id
+        reply_to_message_id=update.message_id
     )
 
 
