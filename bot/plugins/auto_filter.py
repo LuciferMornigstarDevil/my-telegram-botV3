dda4f665a8e44ff88ec613dc6dd6925ad3e4d45b
Lucifer Moring Star <90172375+Lucifermoringsta@users.noreply.github.com>
2021-09-12 13:38:50 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -62,7 +62,7 @@ async def auto_filter(bot, update):
     if filters:
         results.append(
                 [
-                    InlineKeyboardButton("🎀 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🎀", url=f"https://t.me/{MT_CHANNEL_USERNAME}")
+                    InlineKeyboardButton("😎 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 😎", url=f"https://t.me/{MT_CHANNEL_USERNAME}")
                 ]
             )
         for filter in filters: # iterating through each files
@@ -86,7 +86,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"🎃{file_size}📂{file_name}"
+            button_text = f"🔷{file_size}🔶{file_name}"
             
 
             if file_type == "video":
@@ -158,7 +158,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🪄 𝙿𝚊𝚐𝚎 1/{len_result if len_result < max_pages else max_pages} 🪄", callback_data="ignore")
+            InlineKeyboardButton(f"🌟 𝙿𝚊𝚐𝚎 1/{len_result if len_result < max_pages else max_pages} 🌟", callback_data="ignore")
         ])
         
         
@@ -209,7 +209,7 @@ async def auto_filter(bot, update):
             await bot.send_photo(
                 chat_id = update.chat.id,
                 photo= MASSAGE_PHOTO,
-                caption=f"<b>🎯Total File :- {(len_results)} </b>\n<b>🎁Movie Name :-</b> <code>{query}</code>",
+                caption=f"<b>😎താങ്കൾക്ക് കിട്ടിയ ഈ ഫിൽറ്റർ മെസ്സേജ് കാലാവധി വെറും 3 മിനിറ്റ് മാത്രം :- {(len_results)} </b>\n<b>💥Movie Name :-</b> <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
