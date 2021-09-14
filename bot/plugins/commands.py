8268b973e82b445724622b7ed16e3a882240b073
Lucifer Moring Star <90172375+Lucifermoringsta@users.noreply.github.com>
2021-09-14 12:51:36 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG & @Mrk_YT
+# (c) @AlbertEinsteinTG
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram.errors import UserNotParticipant
 from bot import Translation # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
-from bot import UPDATE_CHANNEL # Update Text Message Channel Update
-from bot import MRK_YT_MASTER
-from bot import MT_GROUP
-from bot import MT_CHANNEL # Main Channel Added
-from bot.motech import MT_BOT_UPDATES
+from pyrogram.errors import UserNotParticipant
+from bot import FORCESUB_CHANNEL
 
 db = Database()
 
-@Client.on_message(filters.command(["start"]) & filters.private, group=1)
-async def start(bot, update):
-    update_channel = UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked out":
-               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 🤣🤣🤣**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="<b>🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭.\n\nനിങ്ങൾക്ക് മൂവീസ് വേണോ? എങ്കിൽ തായെ കാണുന്ന ഞങ്ങളുടെ മെയിൻ ചാനലിൽ ജോയിൻ ചെയ്യുക.😂\nഎന്നിട്ട് ഗ്രൂപ്പിൽ പോയി വീണ്ടും മൂവിയിൽ ക്ലിക് ചെയ്ത് start കൊടുത്തു നോക്കൂ..!😁</b>",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text=" 💢 𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎𝚜 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 💢 ", url=f"https://t.me/{UPDATE_CHANNEL}")]
-              ])
-            )
+@Lucifer_Devil_AD.on_message(filters.command(["start"]) & filters.private, group=1) 
+async def start(bot, update): 
+    update_channel = FORCESUB_CHANNEL 
+    if update_channel: 
+        try: 
+            user = await bot.get_chat_member(update_channel, update.chat.id) 
+            if user.status == "kicked": 
+               await update.reply_text("🤭 Sorry Dude, You are B A N N E D 🤣🤣🤣") 
+               return 
+        except UserNotParticipant: 
+            #await update.reply_text(f"Join @{update_channel} To Use Me") 
+            await update.reply_text( 
+                text=""" <b> 🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭. 
+Do you want Movies? If u want Movies Join our main Channel.❤️ 
+Then go to the Group and click movie button, You Will get ..!😁 
+ 
+⚠️YOU ARE NOT SUBSCRIBED OUR CHANNEL⚠️ 
+ 
+Join on our channel to get movies ✅ 
+⬇️Channel link⬇️ </b>""", 
+                reply_markup=InlineKeyboardMarkup([ 
+                    [ InlineKeyboardButton(text="⚡ Join My Channel⚡️", url=f"https://t.me/{update_channel}")] 
+              ]) 
+            ) 
             return
-        except Exception:
-            await update.reply_text(f"<b>This bot should be the admin on your update channel</b>\n\n<b>💢 ഈ ചാനലിൽ  @{UPDATE_CHANNEL} ബോട്ടിനെ അഡ്മിൻ ആക്. എന്നിട്ട് /start കൊടുക്</b>\n\n<b>🗣️ any Doubt @Mo_Tech_Group</b>")
-            return  
     try:
         file_uid = update.command[1]
     except IndexError:
@@ -54,7 +54,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption,
+                caption = f"<code>📁{file_name}</code>\n \n<b>Group :-  @ADMOVEIAD</b><code></code>\n \n<b>Channel :-  @ADMOVEI</b>",                
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -62,33 +62,61 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '♻️Join Main Channel♻️', url=f"{MT_CHANNEL}"
+                                    '🎸 Join Our Group 🎸', url="https://t.me/ADMOVEIAD"
                                 )
                         ],
                         [
                             InlineKeyboardButton
                                 (
-                                    '⭕️ JOIN GROOP ⭕️', url=f"ADMOVEIAD"
+                                    '🧩 Share Group 🧩', url="https://t.me/share/url?url=https://t.me/ADMOVEI"
                                 )
+                        ],
+                        [
+
+                            InlineKeyboardButton
+
+                                (
+
+                                    '💋 New Movies 💋', url="https://t.me/ADMOVEIAD"
+
+                                )
+
                         ]
+                       
                     ]
                 )
             )
 
         elif file_type == "video":
         
-            await update.bot.send_video(
+            await bot.send_video(
                 chat_id=update.chat.id,
                 video = file_id,
-                caption = caption,
+                caption = f"<code>📁{file_name}</code>\n \n<b>Group :-  @ADMOVEIAD</b><code></code>\n \n<b>Channel :-  @ADMOVEI</b>",                 
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    '🔥 JOIN MY CANNEIL 🔥', url="https://t.me/ADMOVEI"
+                                    '🎸 Join Our Group 🎸', url="https://t.me/ADMOVEIAD")
+                        ],
+                        [
+                            InlineKeyboardButton
+                                (
+                                    '🧩 Share Group 🧩', url="https://t.me/share/url?url=https://t.me/ADMOVEIAD"
                                 )
+                        ],
+                        [
+
+                            InlineKeyboardButton
+
+                                (
+
+                                    '💋 New Movies 💋', url="https://t.me/ADMOVEI"
+
+                                )
+
                         ]
                     ]
                 )
@@ -96,18 +124,35 @@ async def start(bot, update):
             
         elif file_type == "audio":
         
-            await update.bot.send_audio(
+            await bot.send_audio(
                 chat_id=update.chat.id,
                 audio = file_id,
-                caption = caption,
+                caption = f"<code>📁{file_name}</code>\n \n<b>Group :-  @ADMOVEIAD</b><code></code>\n \n<b>Channel :-  @ADMOVEIAD</b>",                
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    '💠 JOIN MY GROOP 💠', url="https://t.me/Mo_TECH_YT"
+                                    '🎸 Join Our Group 🎸', url="https://t.me/ADMOVEIAD"
                                 )
+                        ],
+                        [
+                            InlineKeyboardButton
+                                (
+                                    '🧩 Share Group 🧩', url="https://t.me/share/url?url=https://t.me/ADMOVEIAD
+                                )
+                        ],
+                        [
+
+                            InlineKeyboardButton
+
+                                (
+
+                                    '💋 New Movies 💋', url="https://t.me/ADMOVEI"
+
+                                )
+
                         ]
                     ]
                 )
@@ -119,15 +164,15 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('🔻Creater🔺', url=f'https://t.me/{MRK_YT_MASTER}'),
-        InlineKeyboardButton('No Help 😂', callback_data="help")
+        InlineKeyboardButton('🎯ɢʀᴏᴜᴘ🎯', url='https://t.me/ADMOVEIAD'),
+        InlineKeyboardButton('🎯ᴄʜᴀɴɴᴇʟ🎯', url ='https://t.me/ADMOVEI')
     ],[
-        InlineKeyboardButton('✅️Group✅️', url=f'{MT_GROUP}'),
-        InlineKeyboardButton('➡️Channel⬅️', url=f'{MT_CHANNEL}')
+        InlineKeyboardButton('🎯ᴄʜᴀɴɴᴇʟ🎯', url='https://t.me/ADMOVEI')
     ],[
-        InlineKeyboardButton('💜 𝗡𝗢𝗪 𝗝𝗢𝗜𝗡  𝗠𝗬 𝗚𝗥𝗢𝗨𝗣  💜', url='https://t.me/ADMOVEIAD')
+        InlineKeyboardButton('🎯My Developer🎯', url='https://t.me/Lucifer_Devil_AD' )
+    ],[
+        InlineKeyboardButton('Help ⚙', callback_data="help")   
     ]]
-    
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await bot.send_message(
@@ -140,13 +185,13 @@ async def start(bot, update):
     )
 
 
-@Client.on_message(filters.command(["help"]) & filters.private, group=1)
+@Lucifer_Devil_AD.on_message(filters.command(["help"]) & filters.private, group=1)
 async def help(bot, update):
     buttons = [[
-        InlineKeyboardButton('🏠 𝙷𝚘𝚖𝚎', callback_data='start'),
-        InlineKeyboardButton('𝙰𝚋𝚘𝚞𝚝 🚩', callback_data='about')
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('About 🚩', callback_data='about')
     ],[
-        InlineKeyboardButton('🔐 𝙲𝚕𝚘𝚜𝚎 🔐', callback_data='close')
+        InlineKeyboardButton('Close 🔐', callback_data='close')
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -160,16 +205,11 @@ async def help(bot, update):
     )
 
 
-@Client.on_message(filters.command(["about"]) & filters.private, group=1)
+@Lucifer_Devil_AD.on_message(filters.command(["about"]) & filters.private, group=1)
 async def about(bot, update):
     
     buttons = [[
-        InlineKeyboardButton('🔥 JOIN  GROOP 🔥', url='https://t.me/ADMOVEIAD'),
-        InlineKeyboardButton('Skp KP👤', url='https://t.me/Skp_Kp')
-    ],[
-        InlineKeyboardButton('👤 AlbertEinstein 👤', url='https://t.me/AlbertEinsteinTG')
-    ],[
-        InlineKeyboardButton('🏠 Home', callback_data='start'),
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
         InlineKeyboardButton('Close 🔐', callback_data='close')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
